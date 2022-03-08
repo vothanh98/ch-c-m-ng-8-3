aa7351ef4f51028958135a571b9fbaff29503ef0
vothanh98 <101181584+vothanh98@users.noreply.github.com>
2022-03-08 18:23:01 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Các bạn nữ';
+const giftUrl = 'https://www.facebook.com/vothanh1998114/';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng các bạn nữ';
+const contentCard = 'Chúc các bạn một ngày mùng 8/3 thật ý nghĩa, vui tươi ngập tràn hạnh phúc. Chúc các bạn luôn cười tươi, cười duyên, cười e thẹn, cười trẻ trung, ngập tràn các kiểu cười trong ngày hôm nay!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
